6b72f60d80bafa99df6eae83da9d6d6853586c03
Roddy <robotrods@gmail.com>
2020-01-27 20:16:18 -0500

    edited, formatted, deleted unwanted syntax

--- Tkinter - 2 - Organizing your Layout comments area 1.py
+++ Tkinter - 2 - Organizing your Layout comments area 1.py
@@ -1,10 +1,13 @@
 from tkinter import *
 
-f = Tk(); f1=Frame(f);
-f1.pack(side=LEFT);
-name= input("Enter name: ");
-color= input("Enter color: ");
-bgcolor= input("Back ground Color: ")
-ta=Button(f1,text=name,fg=color,bg=bgcolor);
-ta.pack();
-f.mainloop();
+print('\nEnter Name, color and background color, you want on your button...\n')
+f = Tk()
+f1 = Frame(f)
+f1.pack(side=LEFT)
+name = input("Enter name: ")
+color = input("Enter color: ")
+bgcolor = input("Back ground Color: ")
+ta = Button(f1, text=name, fg=color, bg=bgcolor)
+ta.pack()
+
+f.mainloop()
